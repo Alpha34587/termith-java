71ecc465fb69e1cfdfbbef11fec37fccf102fa63
Simon Meoni <simonmeoni@aol.com>
2016-08-23 11:36:32 +0200

    write TextExtractorTest

--- src/test/java/module/TextExtractorTest.java
+++ src/test/java/module/TextExtractorTest.java
@@ -1,16 +1,29 @@
 package module;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by Simon Meoni on 22/08/16.
  */
 public class TextExtractorTest {
-    @Test
-    public void xsltTransformation() throws Exception {
+    TextExtractor textExtractor;
+    @Before
+    public void setUp(){
+        textExtractor = new TextExtractor(new File("src/test/resources/corpus/xml/file1.xml"));
+    }
 
+    @Test
+    public void testXsltTransformation() throws Exception {
+        Assert.assertEquals("the extracted text must be equal",
+                String.join("\n", Files.readAllLines(Paths.get("src/test/resources/corpus/txt/file1.txt"))),
+                textExtractor.XsltTransformation().toString()
+        );
     }
 
 }
