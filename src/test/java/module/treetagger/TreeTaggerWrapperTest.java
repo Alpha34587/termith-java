c3153ca8ffae760f2391ae106ed40f0a700e5c7e
Simon Meoni <simonmeoni@aol.com>
2016-09-02 17:54:58 +0200

    add test wrapper prototype

---
+++ src/test/java/module/treetagger/TreeTaggerWrapperTest.java
@@ -0,0 +1,24 @@
+package module.treetagger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Simon Meoni
+ *         Created on 02/09/16.
+ */
+public class TreeTaggerWrapperTest {
+
+    @Before
+    public void setUp(){
+        String treeTaggerHome = "/home/smeoni/Documents/partage-Termith/scripts/logiciels/treetagger/";
+//        TreeTaggerWrapper treeTaggerWrapper = new TreeTaggerWrapper("",treeTaggerHome,
+//                new TreeTaggerParameter(false,"en", "treeTaggerHome"));
+
+    }
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+}
