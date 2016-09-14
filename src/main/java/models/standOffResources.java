e07584660e164ee109cbc3fb758d4e86ed3e7989
Simon Meoni <simonmeoni@aol.com>
2016-09-14 15:43:53 +0200

    add morphosyntax serialization

--- src/main/java/models/standOffResources.java
+++ src/main/java/models/standOffResources.java
@@ -1,8 +1,33 @@
 package models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * @author Simon Meoni
  *         Created on 14/09/16.
  */
 public class standOffResources {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(standOffResources.class.getName());
+    private final static String PATH = "src/main/resources/standoff/";
+
+    public final static String MS_LIST_ANNOTATION = readFiles(PATH + "ms-list-annotation.xml");
+    public final static String MS_SPAN = readFiles(PATH + "ms-span.xml");
+    public final static String MS_TEI_HEADER = readFiles(PATH + "ms-tei-header.xml");
+    public final static String STANDOFF = readFiles(PATH + "standoff.xml");
+
+
+    private static String readFiles(String path)  {
+        try {
+            return String.join("\n",Files.readAllLines(Paths.get(path)));
+        } catch (IOException e) {
+            LOGGER.error("cannot read resources",e);
+        }
+        return "";
+    }
 }
--- src/main/java/module/tools/Exporter.java
+++ src/main/java/module/tools/Exporter.java
@@ -1,5 +1,6 @@
 package module.tools;
 
+import models.MorphoSyntaxOffsetId;
 import models.TermithIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,9 +10,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import static models.standOffResources.*;
 
 /**
  * This class is used to execute the different output result of the TermithText class
@@ -59,10 +65,13 @@ public class Exporter {
         public void run() {
 
             LOGGER.info("writing : " + termithIndex.getOutputPath() + "/" + key + ".xml");
-            int startText = value.indexOf("<text>");
-            int endText = value.indexOf("</TEI>");
-            value.delete(startText,endText);
-            value.insert(startText,termithIndex.getTokenizeTeiBody().get(key));
+            insertStandOff();
+            insertBody();
+            writeFile();
+
+        }
+
+        private void writeFile() {
             try {
                 BufferedWriter bufferedWriter =
                         Files.newBufferedWriter(Paths.get(termithIndex.getOutputPath() + "/" + key + ".xml"));
@@ -72,7 +81,49 @@ public class Exporter {
             } catch (IOException e) {
                 LOGGER.error("Some errors during files writing",e);
             }
+        }
+
+        private void insertBody() {
+            if (termithIndex.getTokenizeTeiBody().containsKey(key)){
+                int startText = value.indexOf("<text>");
+                int endText = value.indexOf("</TEI>");
+                value.delete(startText,endText);
+                value.insert(startText,termithIndex.getTokenizeTeiBody().get(key));
+            }
+        }
+
+        private void insertStandOff() {
+            int startText = value.indexOf("</teiHeader>");
+            if (termithIndex.getMorphoSyntaxStandOff().containsKey(key))
+                value.insert(startText,serializeMorphosyntax(termithIndex.getMorphoSyntaxStandOff().get(key)));
 
         }
+
+        private StringBuffer serializeMorphosyntax(List<MorphoSyntaxOffsetId> morphoSyntaxOffsetIds) {
+            StringBuffer standoff = new StringBuffer();
+            Deque<MorphoSyntaxOffsetId> morphoDeque = new ArrayDeque<>(morphoSyntaxOffsetIds);
+
+            standoff.append(STANDOFF.split("\n")[0].replace("@type", "wordForms")).append("\n");
+            standoff.append(MS_TEI_HEADER).append("\n");
+            standoff.append(MS_LIST_ANNOTATION.split("\n")[0]).append("\n");
+            while (!morphoDeque.isEmpty()){
+                MorphoSyntaxOffsetId token = morphoDeque.poll();
+                standoff.append(
+                        MS_SPAN.replace("@target",serializeId(token.getIds()))
+                                .replace("@lemma",token.getLemma())
+                                .replace("@pos", token.getTag())
+                ).append("\n");
+            }
+            standoff.append(MS_LIST_ANNOTATION.split("\n")[1]).append("\n");
+            standoff.append(STANDOFF.split("\n")[1]).append("\n");
+
+            return standoff;
+        }
+
+        private String serializeId(List<Integer> ids) {
+            return ids.stream().map(e ->  "t" + e.toString()).reduce(" ", String::join);
+        }
     }
+
+
 }
