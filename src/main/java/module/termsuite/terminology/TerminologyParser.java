f48a6a311532c6dfa3596f131d4fad182d129777
Simon Meoni <simonmeoni@aol.com>
2016-09-14 17:20:29 +0200

    add resources and test for terminology

--- src/main/java/module/termsuite/terminology/TerminologyParser.java
+++ src/main/java/module/termsuite/terminology/TerminologyParser.java
@@ -1,6 +1,12 @@
 package module.termsuite.terminology;
 
+import models.TerminologyOffetId;
+
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Simon Meoni
@@ -8,11 +14,28 @@ import java.nio.file.Path;
  */
 public class TerminologyParser {
     private Path path;
-
+    private Map<String,List<TerminologyOffetId>> standOffTerminology;
+    private Map<String,String> idSource;
     public TerminologyParser(Path path) {
         this.path = path;
+        this.standOffTerminology = new ConcurrentHashMap<>();
+        this.idSource = new HashMap<>();
+    }
+
+    public Map<String, List<TerminologyOffetId>> getStandOffTerminology() {
+        return standOffTerminology;
     }
 
     public void execute() {
+        extractInputSource();
+        extractTerm();
+    }
+
+    private void extractTerm() {
+
+    }
+
+    private void extractInputSource() {
+
     }
 }
---
+++ src/test/java/module/termsuite/terminology/TerminologyParserTest.java
@@ -0,0 +1,22 @@
+package module.termsuite.terminology;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Simon Meoni
+ *         Created on 14/09/16.
+ */
+public class TerminologyParserTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+}
