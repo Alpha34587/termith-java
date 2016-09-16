8a3ae5851711b1d222c9031d2d3e0f017b0ffaed
Simon Meoni <simonmeoni@aol.com>
2016-09-16 11:19:50 +0200

    rewrite Init phase

--- src/main/java/thread/Initializer.java
+++ src/main/java/thread/Initializer.java
@@ -1,15 +1,14 @@
 package thread;
 
 import models.TermithIndex;
-import module.extractor.TextExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import worker.InitCorpusWorker;
+import worker.TextExtractorWorker;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -25,21 +24,15 @@ public class Initializer {
     private static final Logger LOGGER = LoggerFactory.getLogger(Initializer.class);
     private static final int DEFAULT_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 
+    private TermithIndex termithIndex;
     private ExecutorService executor;
-    private Path base;
-    private Map<String, StringBuffer> extractedText;
-    private Map<String, StringBuffer> xmlCorpus;
-
-    public Initializer() {
-        extractedText = new ConcurrentHashMap<>();
-        xmlCorpus = new ConcurrentHashMap<>();
-    }
+    private CountDownLatch corpusCnt;
 
     /**
      * a constructor of initialize who take on parameter a folder path with xml files
      * @param termithIndex the input folder
      */
-    public Initializer(TermithIndex termithIndex) {
+    public Initializer(TermithIndex termithIndex) throws IOException {
         this(DEFAULT_POOL_SIZE, termithIndex);
     }
 
@@ -48,33 +41,12 @@ public class Initializer {
      * @param poolSize number of worker
      * @param termithIndex the input of folder
      */
-    public Initializer(int poolSize, TermithIndex termithIndex) {
-        base = termithIndex.getBase();
-        executor = Executors.newFixedThreadPool(poolSize);
-        extractedText = termithIndex.getExtractedText();
-        xmlCorpus = termithIndex.getXmlCorpus();
+    public Initializer(int poolSize, TermithIndex termithIndex) throws IOException {
+        this.termithIndex = termithIndex;
+        this.executor = Executors.newFixedThreadPool(poolSize);
+        corpusCnt  = new CountDownLatch(
+                (int)Files.list(termithIndex.getBase()).count());
     }
-
-    /**
-     * @return return the list of extracted text extracts by the process
-     */
-    public Map<String, StringBuffer> getExtractedText() {
-        return extractedText;
-    }
-
-
-    /**
-     * add a text manually to the corpus
-     * @param id the name of the corpus
-     * @param content the content of the corpus
-     */
-    public void addText(String id, StringBuffer content){extractedText.put(id,content);}
-
-
-    /**
-     * @return return the base corpus
-     */
-    public Map<String, StringBuffer> getXmlCorpus() { return xmlCorpus; }
 
     /**
      * execute the extraction text task with the help of inner InitializerWorker class
@@ -82,42 +54,20 @@ public class Initializer {
      * @throws InterruptedException
      */
     public void execute() throws IOException, InterruptedException {
-        LOGGER.info("Starting initilization of files of folder: " + this.base);
-        Files.list(base).forEach(p -> executor.submit(new InitialiazerWorker((Path) p)));
-        LOGGER.info("Waiting executors to finish");
+        Files.list(termithIndex.getBase()).forEach(
+                p -> {
+                    executor.submit(new TextExtractorWorker(p,termithIndex));
+                    executor.submit(new InitCorpusWorker(p, termithIndex,corpusCnt));
+                }
+
+        );
+        LOGGER.info("Waiting initCorpusWorker executors to finish");
+        corpusCnt.await();
+        LOGGER.info("Waiting initCorpusWorker executors to finish");
+        corpusCnt.await();
+        LOGGER.info("initCorpusWorker finished");
         executor.shutdown();
         executor.awaitTermination(1L, TimeUnit.DAYS);
     }
 
-    private class InitialiazerWorker implements Runnable {
-
-        private Path path;
-
-        /**
-         * constructor of the class the parameter path is the path of the file that we want to treated
-         * @param path
-         */
-        InitialiazerWorker(Path path) {
-            this.path = path;
-        }
-
-        /**
-         * this override method run call the extractor text method
-         */
-        @Override
-        public void run() {
-            try {
-                LOGGER.info("Extracting text of file: " + this.path);
-                TextExtractor textExtractor = new TextExtractor(path.toFile());
-                StringBuffer buffer = textExtractor.xsltTransformation();
-                extractedText.put(path.getFileName().toString().replace(".xml", ""), buffer);
-                xmlCorpus.put(path.getFileName().toString().replace(".xml", ""), new StringBuffer(
-                        String.join("\n",Files.readAllLines(path))
-                ));
-                LOGGER.info("Extraction done for file: " + this.path);
-            } catch (IOException e) {
-                LOGGER.info("File Exception",e);
-            }
-        }
-    }
 }
