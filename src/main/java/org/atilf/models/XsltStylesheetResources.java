5462a1151889ba046ab17461b71250da1272c1bf
Simon <simonmeoni@aol.com>
2016-09-21 01:15:17 +0200

    write resource initilaizer but not work when building jar

--- src/main/java/org/atilf/models/XsltStylesheetResources.java
+++ src/main/java/org/atilf/models/XsltStylesheetResources.java
@@ -1,9 +1,20 @@
 package org.atilf.models;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ClassRelativeResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static eu.project.ttc.utils.TermSuiteConstants.R;
 
 /**
  * @author Simon Meoni
@@ -11,5 +22,15 @@ import java.io.File;
  */
 public class XsltStylesheetResources {
     public static final TransformerFactory FACTORY = TransformerFactory.newInstance();
-    public static final Source EXTRACT_TEXT = new StreamSource(new File("src/main/resources/xsl/ExtractText.xsl"));
+    public static Source EXTRACT_TEXT;
+
+    static {
+        EXTRACT_TEXT = null;
+
+        try {
+            EXTRACT_TEXT = new StreamSource(new File(ResourceLoader.class.getClassLoader().getResource("./xsl/ExtractText.xsl").toURI()));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
