2d817080402c65027b90370ce993eecc4769e8fe
Simon Meoni <simonmeoni@aol.com>
2016-10-20 17:22:21 +0200

    add element from expected corpus

--- src/test/java/org/atilf/module/disambiguisation/GlobalCorpusTest.java
+++ src/test/java/org/atilf/module/disambiguisation/GlobalCorpusTest.java
@@ -25,7 +25,30 @@ public class GlobalCorpusTest {
         corpus1 = new GlobalCorpus("src/test/resources/corpus/tei/test3.xml",observedCorpus);
         corpus2 = new GlobalCorpus("src/test/resources/corpus/tei/test4.xml",observedCorpus);
 
-
+        expectedCorpus.add("ce PRO:DEM");
+        expectedCorpus.add("article NOM");
+        expectedCorpus.add("présenter VER:pres");
+        expectedCorpus.add("un DET:ART");
+        expectedCorpus.add("étude NOM");
+        expectedCorpus.add("comparer VER:pper");
+        expectedCorpus.add("du PRP:det");
+        expectedCorpus.add("donnée NOM");
+        expectedCorpus.add("archéo-ichtyofauniques ADJ");
+        expectedCorpus.add("livrer VER:pper");
+        expectedCorpus.add("par PRP");
+        expectedCorpus.add("deux NUM");
+        expectedCorpus.add("ce PRO:DEM");
+        expectedCorpus.add("article NOM");
+        expectedCorpus.add("présenter VER:pres");
+        expectedCorpus.add("un DET:ART");
+        expectedCorpus.add("étude NOM");
+        expectedCorpus.add("comparer VER:pper");
+        expectedCorpus.add("du PRP:det");
+        expectedCorpus.add("donnée NOM");
+        expectedCorpus.add("archéo-ichtyofauniques ADJ");
+        expectedCorpus.add("rouge VER:pper");
+        expectedCorpus.add("chien PRP");
+        expectedCorpus.add("deux NUM");
     }
 
     @Test
