1a61347f0032ccc422a9e0c0b296ae36c30502cf
Simon Meoni <simonmeoni@aol.com>
2016-10-25 17:18:29 +0200

    add Test Method

---
+++ src/test/java/org/atilf/module/tools/DisambTeiWriterTest.java
@@ -0,0 +1,23 @@
+package org.atilf.module.tools;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Simon Meoni
+ *         Created on 25/10/16.
+ */
+public class DisambTeiWriterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+}
