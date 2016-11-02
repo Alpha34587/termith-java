68089a91e2730df9faa352e3bec1cdd1aa76d1cf
Simon Meoni <simonmeoni@aol.com>
2016-11-02 12:26:50 +0100

    create empty

---
+++ src/test/java/org/atilf/module/disambiguisation/DisambXslTransformerTest.java
@@ -0,0 +1,17 @@
+package org.atilf.module.disambiguisation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Simon Meoni
+ *         Created on 02/11/16.
+ */
+public class DisambXslTransformerTest {
+    @Test
+    public void xsltTransformation() throws Exception {
+
+    }
+
+}
