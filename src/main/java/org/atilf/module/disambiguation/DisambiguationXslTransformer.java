5b945779e8cb63af05f84f5957c8ede4577f4e30
Simon Meoni <simonmeoni@aol.com>
2016-11-14 17:26:47 +0100

    write documentation for DisambiguationXslTransformer

--- src/main/java/org/atilf/module/disambiguation/DisambiguationXslTransformer.java
+++ src/main/java/org/atilf/module/disambiguation/DisambiguationXslTransformer.java
@@ -14,32 +14,64 @@ import java.util.concurrent.CountDownLatch;
 /**
  * @author Simon Meoni
  *         Created on 02/11/16.
+ *         the DisambiguationXslTransformer convert tei file into xml working file format.
+ *         There are some example on the folder : src/test/resources/corpus/disambiguation/transform-tei.
  */
 public class DisambiguationXslTransformer extends TextExtractor {
     private TermithIndex _termithIndex;
     private CountDownLatch _transformCounter;
     private static final Logger LOGGER = LoggerFactory.getLogger(DisambiguationXslTransformer.class.getName());
 
+    /**
+     * constructor of DisambiguationXslTransformer module
+     * @param file input tei file
+     * @param xslResources the xslResource object who have as field the parse xsl stylesheet
+     *                     used to convert the input file
+     */
     public DisambiguationXslTransformer(File file, XslResources xslResources) {
         super(file,xslResources);}
 
+    /**
+     * constructor of DisambiguationXslTransformer module
+     * @param transformCounter the transformCounter in an object of java.util.concurrent. When the transformation
+     *                         is performed the counter is decreased. This counter is used in the ContextLexiconThread
+     *                         in order to wait that the transformation is performed for all the corpus
+     * @param file input tei file
+     * @param xslResources the xslResource object who have as field the parse xsl stylesheet
+     *                     used to convert the input file
+     */
     public DisambiguationXslTransformer(File file, CountDownLatch transformCounter, TermithIndex termithIndex, XslResources xslResources){
         super(file,xslResources);
         _transformCounter = transformCounter;
         _termithIndex = termithIndex;
     }
 
+    /**
+     * this method call the inherited method execute from textExtractor. the method execute transform the file
+     * and return a StringBuffer with the result. this result is wrote in a file and the path is retained on a map in a
+     * termithIndexObject
+     */
     @Override
     public void run() {
         try {
             LOGGER.info("convert xml file: " + _file.getAbsolutePath());
             _termithIndex.getDisambiguationTransformedFile().put(
-                    FilesUtils.nameNormalizer(_file.getName()),
+                    FilesUtils.nameNormalizer(
+                    /*
+                    key of the entry
+                     */
+                            _file.getName()),
+                    /*
+                    transform and write file
+                     */
                     FilesUtils.writeXml(
                             this.execute(),
                             TermithIndex.getOutputPath(),
                             _file.getName())
             );
+            /*
+            decrease the counter
+             */
             _transformCounter.countDown();
             LOGGER.info("Extraction done for file: " + _file);
         } catch (IOException e) {
