f3de9b5f9da9ad2373b461e8b24fd92f4728f63f
Simon Meoni <simonmeoni@aol.com>
2016-11-15 15:02:14 +0100

    write documentation for textExtractor

--- src/main/java/org/atilf/module/extractor/TextExtractor.java
+++ src/main/java/org/atilf/module/extractor/TextExtractor.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 /**
- * The textExtractor class is used extract the plain text of an XML _file
+ * The textExtractor class is used extract the plain text of a tei file
  * @author Simon Meoni
  * Created on 25/07/16.
  */
@@ -27,9 +27,10 @@ public class TextExtractor implements Runnable{
     protected XslResources _xslResources;
 
     /**
-     * builder for textExtractor
+     * constructor for textExtractor
      * @param file Treated xml/tei _file
-     * @param xslResources
+     * @param xslResources contains the parsed xsl stylesheet
+     * @param termithIndex the termithIndex of a process
      */
     public TextExtractor(File file, TermithIndex termithIndex, XslResources xslResources) {
         _file = file;
@@ -37,17 +38,26 @@ public class TextExtractor implements Runnable{
         _xslResources = xslResources;
     }
 
+    /**
+     * constructor for textExtractor
+     * @param file Treated xml/tei _file
+     * @param xslResources contains the parsed xsl stylesheet
+     */
     public TextExtractor(File file, XslResources xslResources) {
         _file = file;
         _xslResources = xslResources;
     }
 
     /**
-     * this method apply an xsl stylesheet to a _file given in parameter. it extracts the plain text of the xml _file
+     * this method apply an xsl stylesheet to a file given in the _file field. it extracts the plain text of the tei file
      * @return the extracted text
      * @throws IOException throw IO exception of StreamResult variable
      */
     public StringBuilder execute() throws IOException {
+        /*
+        instantiate needed variables for transformation. The StringWriter variable is used to return
+        the result as a StringBuilder variable
+         */
         Source input = new StreamSource(_file);
         Transformer transformer;
         StringWriter stringWriter = new StringWriter();
@@ -55,7 +65,13 @@ public class TextExtractor implements Runnable{
 
         try {
             LOGGER.debug("apply " + _xslResources._stylesheet.toString() + "to xml file" + input.toString());
+            /*
+            get new transformer
+             */
             transformer = _xslResources._factory.newTransformer();
+            /*
+            apply the transformation
+             */
             transformer.transform(input, streamResult);
 
         } catch (TransformerException e) {
@@ -65,15 +81,31 @@ public class TextExtractor implements Runnable{
         return new StringBuilder(stringWriter.getBuffer());
     }
 
+    /*
+    this method call execute method and write the result into the working directory and keep the path
+    into the _termithIndex field
+     */
     @Override
     public void run() {
         try {
             LOGGER.debug("Extracting text of file: " + _file);
-            StringBuilder extractedText = this.execute();
+            /*
+            call execute method
+             */
+            StringBuilder extractedText = execute();
+            /*
+            check if the result of the extraction is not empty
+             */
             if (extractedText.length() != 0) {
+                /*
+                put the result into the extractedText Map
+                 */
                 _termithIndex.getExtractedText().put(FilesUtils.nameNormalizer(_file.toString()),
-                        FilesUtils.writeObject(this.execute(), TermithIndex.getOutputPath()));
+                        FilesUtils.writeObject(extractedText, TermithIndex.getOutputPath()));
             }
+            /*
+            otherwise a log is thrown and the corpusSize is decremented by 1
+             */
             else {
                 LOGGER.info(_file + " has empty body");
                 _termithIndex.setCorpusSize(_termithIndex.getCorpusSize() - 1);
