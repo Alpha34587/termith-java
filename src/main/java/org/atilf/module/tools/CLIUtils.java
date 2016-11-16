c42620aa8e3a9bd0fac2542d5b3cea3b2dd3badc
Simon Meoni <simonmeoni@aol.com>
2016-11-16 10:45:02 +0100

    write CLIUtils javadoc

--- src/main/java/org/atilf/module/tools/CLIUtils.java
+++ src/main/java/org/atilf/module/tools/CLIUtils.java
@@ -5,12 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * CLI tools for cli package
  * @author Simon Meoni
  *         Created on 19/09/16.
  */
 public class CLIUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(CLIUtils.class.getName());
 
+    /**
+     * set log level
+     * @param level the level of log that we want to show during the process
+     */
     public static void setGlobalLogLevel(Level level) {
         ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger)LoggerFactory
                 .getLogger(Logger.ROOT_LOGGER_NAME);
