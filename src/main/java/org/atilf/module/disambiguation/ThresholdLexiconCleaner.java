750297e452251b69d00f1b6152f8970323e22e25
Simon Meoni <simonmeoni@aol.com>
2016-12-05 12:25:10 +0100

    write test unit and write the remained method

--- src/main/java/org/atilf/module/disambiguation/ThresholdLexiconCleaner.java
+++ src/main/java/org/atilf/module/disambiguation/ThresholdLexiconCleaner.java
@@ -2,11 +2,14 @@ package org.atilf.module.disambiguation;
 
 import org.atilf.models.disambiguation.LexiconProfile;
 
+import java.util.Map;
+
 /**
- * Created by simon on 04/12/16.
+ * @author Simon Meoni
+ * Created on 04/12/16.
  */
 public class ThresholdLexiconCleaner implements Runnable {
-    private final LexiconProfile _lexiconProfile;
+    private final Map<String,Float> _coefficientMap;
     private final int _minThreshold;
     private final int _maxThreshold;
     private final int _negMinThreshold;
@@ -14,19 +17,36 @@ public class ThresholdLexiconCleaner implements Runnable {
 
     public ThresholdLexiconCleaner(LexiconProfile lexiconProfile, int minThreshold, int maxThreshold,
                                    int negMinThreshold, int negMaxThreshold) {
-        _lexiconProfile = lexiconProfile;
+        this(
+                lexiconProfile.getSpecCoefficientMap(),
+                minThreshold,
+                maxThreshold,
+                negMinThreshold,
+                negMaxThreshold
+        );
+    }
+
+    public ThresholdLexiconCleaner(Map<String,Float> coefficientMap, int minThreshold, int maxThreshold,
+                                   int negMinThreshold, int negMaxThreshold) {
+        _coefficientMap = coefficientMap;
         _minThreshold = minThreshold;
         _maxThreshold = maxThreshold;
         _negMinThreshold = negMinThreshold;
         _negMaxThreshold = negMaxThreshold;
     }
 
-    public void execute(){
 
+    public void execute(){
+        _coefficientMap.values().removeIf(this::isNotBetweenThreshold);
     }
+
+    public boolean isNotBetweenThreshold(Float value) {
+        return false;
+    }
+
 
     @Override
     public void run() {
-        
+
     }
 }
