41bcbf9ea728dc500b75fa94d0e87d66c8f7274d
Simon Meoni <simonmeoni@aol.com>
2017-01-31 13:51:29 +0100

    add progressEvent class and remove benchmark class

---
+++ src/main/java/org/atilf/monitor/observer/ProgessEvent.java
@@ -0,0 +1,10 @@
+package org.atilf.monitor.observer;
+
+/**
+ * @author Simon Meoni Created on 31/01/17.
+ */
+public class ProgessEvent extends TermithEvent {
+    public ProgessEvent(String name) {
+        super(name);
+    }
+}
