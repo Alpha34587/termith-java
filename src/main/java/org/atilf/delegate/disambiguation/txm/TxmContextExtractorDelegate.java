4d4bd56b62bf1248a0c129ac670597866560e6e1
Simon Meoni <simonmeoni@aol.com>
2017-04-19 12:00:13 +0200

    add delegate txm module

---
+++ src/main/java/org/atilf/delegate/disambiguation/txm/TxmContextExtractorDelegate.java
@@ -0,0 +1,9 @@
+package org.atilf.delegate.disambiguation.txm;
+
+import org.atilf.delegate.Delegate;
+
+/**
+ * Created by smeoni on 19/04/17.
+ */
+public class TxmContextExtractorDelegate extends Delegate{
+}
