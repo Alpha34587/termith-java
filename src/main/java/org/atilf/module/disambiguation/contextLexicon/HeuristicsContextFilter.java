7053bbae5984037686a4bc84c645f7ef75473577
Simon Meoni <simonmeoni@aol.com>
2017-05-16 16:03:03 +0200

    finalize module

--- src/main/java/org/atilf/module/disambiguation/contextLexicon/HeuristicsContextFilter.java
+++ src/main/java/org/atilf/module/disambiguation/contextLexicon/HeuristicsContextFilter.java
@@ -1,19 +1,35 @@
 package org.atilf.module.disambiguation.contextLexicon;
 
 import org.atilf.models.TermithIndex;
+import org.atilf.models.disambiguation.LexiconProfile;
 import org.atilf.module.Module;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Simon Meoni on 16/05/17.
  */
 public class HeuristicsContextFilter extends Module{
 
+
+    private Map<String, LexiconProfile> _contextLexiconMap;
+
     public HeuristicsContextFilter(TermithIndex termithIndex) {
-        super(termithIndex);
+        this(termithIndex.getContextLexicon());
+    }
+
+    HeuristicsContextFilter(Map<String,LexiconProfile> ContextLexiconMap){
+        _contextLexiconMap = ContextLexiconMap;
     }
 
     @Override
     protected void execute() {
-        super.execute();
+        Set<String> entries = new HashSet<>(_contextLexiconMap.keySet());
+        entries.stream()
+                .filter(el -> !_contextLexiconMap.containsKey(el.replace("lexOn","lexOff"))
+                || !_contextLexiconMap.containsKey(el.replace("lexOff","lexOn")))
+                .forEach(el -> _contextLexiconMap.remove(el));
     }
 }
--- src/test/java/org/atilf/module/disambiguation/contextLexicon/HeuristicsContextFilterTest.java
+++ src/test/java/org/atilf/module/disambiguation/contextLexicon/HeuristicsContextFilterTest.java
@@ -1,18 +1,42 @@
 package org.atilf.module.disambiguation.contextLexicon;
 
+import org.atilf.models.disambiguation.LexiconProfile;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Simon Meoni on 16/05/17.
  */
 public class HeuristicsContextFilterTest {
+    public static Map<String,LexiconProfile> _lexiconProfileMap = new HashMap<>();
+    public static HeuristicsContextFilter _heuristicsContextFilter;
+    public static Set<String> _expectedKey = new HashSet<>();
+
     @BeforeClass
-    public void setUp() throws Exception {
+    public static void setUp() throws Exception {
+        _lexiconProfileMap.put("entry1_lexOn",new LexiconProfile());
+        _lexiconProfileMap.put("entry2_lexOn",new LexiconProfile());
+        _lexiconProfileMap.put("entry2_lexOff",new LexiconProfile());
+        _lexiconProfileMap.put("entry3_lexOn",new LexiconProfile());
+        _lexiconProfileMap.put("entry4_lexOn",new LexiconProfile());
+        _heuristicsContextFilter = new HeuristicsContextFilter(_lexiconProfileMap);
+        _expectedKey.add("entry2_lexOn");
+        _expectedKey.add("entry2_lexOff");
     }
 
     @Test
     public void execute() throws Exception {
+        _heuristicsContextFilter.execute();
+        Set<String> observedKey = _lexiconProfileMap.keySet();
+        Assert.assertArrayEquals("the expected and observed key must be equals",
+                observedKey.toArray(),
+                _expectedKey.toArray());
     }
 
 }
